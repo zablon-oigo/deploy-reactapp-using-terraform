90b677832dd864d1cde8d4f8a76d8c7731231628
zablon <zablonoigo51@gmail.com>
2024-08-02 11:48:52 +0300

    adding aos animation

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,20 +8,26 @@ export default function Banner(){
         <div className="container h-full mx-auto">
             <div className="relative flex items-center h-full -space-x-48 lg:-space-x-24">
                <div className="z-10 flex-1 pl-8 text-white lg:pl-0">
-                <h1 className="mb-8 text-white h1">
+                <h1 className="mb-8 text-white h1" data-aos="fade-down" data-aos-delay='500'>
                     {titlePart1} <br/>{' '}
                     <span className="text-primary-200">
                         {titlePart2}
                     </span>
                 </h1>
-                <p className="max-w-[415px] text-body-md lg:text-body-lg mb-8">
+                <p className="max-w-[415px] text-body-md lg:text-body-lg mb-8"
+                 data-aos="fade-down" data-aos-delay='600'
+                >
                     {subtitle}
                 </p>
-                <button className="btn btn-sm lg:btn-lg btn-secondary">
+                <button className="btn btn-sm lg:btn-lg btn-secondary"
+                 data-aos="fade-down" data-aos-delay='700'
+                >
                     {textBtn}
                 </button>
                </div>
-               <div className="flex-1 w-full h-full bg-blue-300 bg-right bg-no-repeat bg-cover bg-banner lg:bg-center">
+               <div className="flex-1 w-full h-full bg-blue-300 bg-right bg-no-repeat bg-cover bg-banner lg:bg-center"
+                data-aos="fade-right" data-aos-delay='900'
+               >
                 <img src="" alt="" className="" />
                </div>
             </div>
