43ca4e50a5d4ae79b59c68a90ad60e0257becb8b
zablon <zablonoigo51@gmail.com>
2024-08-03 20:18:45 +0300

    Adding  small screen nav links

--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import { nav } from '../data'
 
-export default function NavMobile() {
+export default function NavMobile({ navMobile }) {
   return (
-    <nav className=""></nav>
+    <nav
+      className={`lg:hidden w-full bg-neutral-500 fixed top-0 left-0 right-0 -bottom-12 -z-10 overflow-hidden transition-all ${navMobile ? 'min-h-screen' : 'h-0'}`}
+      aria-hidden={!navMobile}
+    >
+      <ul className="flex flex-col items-center justify-center w-full h-full gap-y-8">
+        {nav.map((item, idx) => (
+          <li key={idx}>
+            <a href={item.href} className="text-white text-body-md">
+              {item.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <div className="flex justify-center mt-8 gap-x-8">
+        <button className="text-white btn btn-lg">Log in</button>
+        <button className="btn btn-lg btn-primary">Sign up</button>
+      </div>
+    </nav>
   )
 }
