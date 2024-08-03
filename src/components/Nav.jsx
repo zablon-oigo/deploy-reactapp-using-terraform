cc9d71eab0e0079987d8dba41fc69896bc270a49
zablon <zablonoigo51@gmail.com>
2024-08-03 20:18:19 +0300

    Added Nav Links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,6 +3,19 @@ import { nav } from '../data'
 
 export default function Nav() {
   return (
-    <nav className=""></nav>
+    <nav className="hidden lg:flex">
+      <ul className="flex text-white gap-x-8">
+        {nav.map((item,idx)=>{
+          return(
+            <li className="" key={idx}>
+              <a href={item.href} className="transition hover:text-primary-200 ">
+                {item.name}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+
+    </nav>
   )
 }
