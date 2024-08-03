ee61f7b207bffa207d6627f9a6e9353fb565e0e9
zablon <zablonoigo51@gmail.com>
2024-08-03 20:17:34 +0300

    Styling header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
 import React,{useState,useEffect} from 'react'
 import {header} from '../data'
 import Nav from './Nav';
-import { RiMedal2Fill } from 'react-icons/ri';
+import { RiCloseFill, RiMedal2Fill, RiMenu4Fill } from 'react-icons/ri';
 import NavMobile from './NavMobile';
 export default function Header() {
   const[isActive,setIsActive]=useState(false)
   const{logo,btnLoginText,btnSignupText}=header;
+  const[navMobile,setNavMobile]=useState(false);
+  useEffect(()=>{
+    window.addEventListener('scroll',()=>{
+      window.scrollY>80 ? setIsActive(true):setIsActive(false);
+    })
+  })
   return (
     <>
     <header className={`${isActive?'bg-neutral-500 py-[16px]':'bg-transparent py-[20px]'} fixed max-w-[1440px] z-30 left-0 right-0  mx-auto flex justify-between items-center px-[20px] lg:px-[80px] transition-all duration-300`}>
@@ -14,18 +20,19 @@ export default function Header() {
       </a>
       <Nav/>
       <div className="hidden space-x-4 lg:flex">
-        <button className="">
+        <button className="text-white transition btn btn-sm hover:text-primary-200">
           {btnLoginText}
         </button>
-        <button className="">
+        <button className="btn btn-sm btn-primary">
           {btnSignupText}
         </button>
       </div>
-      <div className="absolute lg:hidden right-4">
-        <RiMedal2Fill className='text-3xl text-primary-200'/>
+      <div className="absolute lg:hidden right-4" onClick={()=>setNavMobile(!navMobile)}>
+        {navMobile?( <RiCloseFill className="text-3xl cursor-pointer text-primary-200"/>):(<RiMenu4Fill className='text-3xl cursor-pointer text-primary-200'/>)}
+        
       
       </div>
-      <NavMobile/>
+      <NavMobile navMobile={navMobile}/>
     </header>
     </>
   )
