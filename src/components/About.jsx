f85f078d3ac88edda4e22c769789153b9069cef0
zablon <zablonoigo51@gmail.com>
2024-08-03 20:52:27 +0300

    About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
-
+import { about } from '../data'
+import {IoIosArrowDropdownCircle} from 'react-icons/io'
 export default function About() {
+  const{title,subtitle1,subtitle2,icon,link}=about;
   return (
-    <div>About</div>
+    <section className="">
+      <div className='container mx-auto'>
+      <div className="secton-title-group">
+        <img src={icon} alt="" className="" />
+        <h2 className="h2 section-title">
+          {title}
+          <span className="text-primary-200">.</span>
+        </h2>
+      </div>
+      <p className="">
+        {subtitle1}
+      </p>
+      <p className="">
+        {subtitle2}
+      </p>
+    </div>
+    </section>
   )
 }
