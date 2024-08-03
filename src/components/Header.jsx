76de26e94d8861aeb7300ac667a14f614dd569ca
zablon <zablonoigo51@gmail.com>
2024-08-03 09:25:09 +0300

    Nav mobile styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,12 +8,12 @@ export default function Header() {
   const{logo,btnLoginText,btnSignupText}=header;
   return (
     <>
-    <header className={`${isActive?'bg-neutral-500 py-[16px]':'bg-transparent py-[20px]'} fixed max-w-[1440px] z-30 left-0 right-0  mx-auto flex justify-between`}>
+    <header className={`${isActive?'bg-neutral-500 py-[16px]':'bg-transparent py-[20px]'} fixed max-w-[1440px] z-30 left-0 right-0  mx-auto flex justify-between items-center px-[20px] lg:px-[80px] transition-all duration-300`}>
       <a href="" className="">
-        <img src={logo} alt="" className="" />
+        <img src={logo} alt="" className="h-[30px]" />
       </a>
       <Nav/>
-      <div className="">
+      <div className="hidden space-x-4 lg:flex">
         <button className="">
           {btnLoginText}
         </button>
@@ -21,8 +21,8 @@ export default function Header() {
           {btnSignupText}
         </button>
       </div>
-      <div className="absolute top-0 lg:hidden right-4">
-        <RiMedal2Fill/>
+      <div className="absolute lg:hidden right-4">
+        <RiMedal2Fill className='text-3xl text-primary-200'/>
       
       </div>
       <NavMobile/>
