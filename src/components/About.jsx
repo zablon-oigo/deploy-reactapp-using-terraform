41df434507e2e00c8acbda3b8422450036b51994
zablon <zablonoigo51@gmail.com>
2024-08-03 20:55:31 +0300

    Styling about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,16 +4,16 @@ import {IoIosArrowDropdownCircle} from 'react-icons/io'
 export default function About() {
   const{title,subtitle1,subtitle2,icon,link}=about;
   return (
-    <section className="">
+    <section className="bg-pink-200 py-[80px] md:py-[110px] lg:pt-[140px] lg:pb-[180px] ">
       <div className='container mx-auto'>
-      <div className="secton-title-group">
+      <div className="justify-start secton-title-group">
         <img src={icon} alt="" className="" />
         <h2 className="h2 section-title">
           {title}
           <span className="text-primary-200">.</span>
         </h2>
       </div>
-      <p className="">
+      <p className="md:text-body-md">
         {subtitle1}
       </p>
       <p className="">
