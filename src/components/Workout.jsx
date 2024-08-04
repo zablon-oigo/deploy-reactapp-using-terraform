406b66c5b551c35604e5e534b550248a3d80a0ca
zablon <zablonoigo51@gmail.com>
2024-08-04 23:10:13 +0300

    Destructuring items

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
-
+import { workouts } from '../data'
 export default function Workout() {
+    const{title,icon}=workouts;
   return (
-    <div>Workout</div>
+    <div className='bg-pink-200'>
+        <div className="">
+            <img src={icon} alt="" className="" />
+            <h2 className="">
+                {title}
+            </h2>
+        </div>
+    </div>
   )
 }
