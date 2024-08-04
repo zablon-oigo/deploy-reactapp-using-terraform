bac1577166deac6d26236d261680afa379550617
zablon <zablonoigo51@gmail.com>
2024-08-04 23:11:34 +0300

    Styling workout component

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -3,10 +3,10 @@ import { workouts } from '../data'
 export default function Workout() {
     const{title,icon}=workouts;
   return (
-    <div className='bg-pink-200'>
-        <div className="">
+    <div className='section'>
+        <div className="section-title-group max-w-[540px] mx-auto   px-4 lg:px-0">
             <img src={icon} alt="" className="" />
-            <h2 className="">
+            <h2 className="h2 section-title">
                 {title}
             </h2>
         </div>
