c88f0fb4919bbaaf60801d684581f3ff821f8dc6
zablon <zablonoigo51@gmail.com>
2024-08-04 22:56:03 +0300

    Adding animation effect

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,14 +13,14 @@ export default function About() {
           <span className="text-primary-200">.</span>
         </h2>
       </div>
-      <p className="mb-12 md:text-body-md">
+      <p className="mb-12 md:text-body-md"data-aos='fade-up' data-aos-delay='200'>
         {subtitle1}
       </p>
-      <p className="mb-8 md:text-body-md">
+      <p className="mb-8 md:text-body-md"data-aos='fade-up' data-aos-delay='300'>
         {subtitle2}
       </p>
     </div>
-    <div className="">
+    <div className=""data-aos='fade-up' data-aos-delay='400'>
       <a href="" className="flex items-center transition-all link gap-x-4 hover:gap-x-6 ">
         {link}
         <IoIosArrowDroprightCircle className='text-2xl'/>
