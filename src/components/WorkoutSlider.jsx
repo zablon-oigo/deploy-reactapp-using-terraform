b8bd8150b46b81f4881e78213350729792072b0e
zablon <zablonoigo51@gmail.com>
2024-08-06 08:48:25 +0300

    Adding workout slider component

--- src/components/WorkoutSlider.jsx
+++ src/components/WorkoutSlider.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import {workouts} from '../data'
+import {Swiper,SwiperSlide} from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import '../WorkoutSlider.css'
 
+import {} from 'swiper'
 export default function WorkoutSlider() {
+  const{programs}=workouts;
   return (
-    <div>WorkoutSlider</div>
+    <Swiper>
+      {programs.map((program,idx)=>{
+        return <SwiperSlide>
+          slide
+        </SwiperSlide>
+      })}
+    </Swiper>
   )
 }
