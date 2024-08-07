1a1a615470d54c2a423926994af5cc6d7cf854e3
zablon <zablonoigo51@gmail.com>
2024-08-07 15:52:31 +0300

    Adding AOS animation

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -5,7 +5,7 @@ export default function Workout() {
     const{title,icon}=workouts;
   return (
     <section className='section'>
-        <div className="section-title-group max-w-[540px] mx-auto   px-4 lg:px-0">
+        <div className="section-title-group max-w-[540px] mx-auto   px-4 lg:px-0" data-aos='fade-up' data-aos-delay='200'>
             <img src={icon} alt="" className="" />
             <h2 className="h2 section-title">
                 {title}
