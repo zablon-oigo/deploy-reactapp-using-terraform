7bdda1267d75c51f7e13cc65ec908c70fa499f47
zablon <zablonoigo51@gmail.com>
2024-08-07 15:22:21 +0300

    Increasing padding on X-axis

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,11 +4,11 @@ import {IoIosArrowDroprightCircle} from 'react-icons/io'
 export default function About() {
   const{title,subtitle1,subtitle2,icon,link}=about;
   return (
-    <section className=" py-[80px] md:py-[110px] lg:pt-[140px] lg:pb-[180px] ">
+    <section className=" py-[20px] md:py-[110px] md:px-0 px-[20px] lg:pt-[140px] lg:pb-[180px] ">
       <div className='container mx-auto'>
       <div className="justify-start secton-title-group" data-aos='fade-up' data-aos-delay='100'>
         <img src={icon} alt="" className="" />
-        <h2 className="h2 section-title">
+        <h2 className="mb-2 h2 section-title">
           {title}
           <span className="text-primary-200">.</span>
         </h2>
@@ -20,7 +20,7 @@ export default function About() {
         {subtitle2}
       </p>
     </div>
-    <div className=""data-aos='fade-up' data-aos-delay='400'>
+    <div className="md:ml-28"data-aos='fade-up' data-aos-delay='400'>
       <a href="" className="flex items-center transition-all link gap-x-4 hover:gap-x-6 ">
         {link}
         <IoIosArrowDroprightCircle className='text-2xl'/>
