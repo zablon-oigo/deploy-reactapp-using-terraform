2d6a1783ab0689c8469335581350227818da48b3
zablon <zablonoigo51@gmail.com>
2024-08-07 15:53:02 +0300

    Adding  slides per view

--- src/components/WorkoutSlider.jsx
+++ src/components/WorkoutSlider.jsx
@@ -6,16 +6,27 @@ import 'swiper/css/navigation'
 import '../workoutSlider.css'
 
 import {} from 'swiper'
+import { Navigation } from 'swiper/modules'
 export default function WorkoutSlider() {
   const{programs}=workouts;
   return (
-    <Swiper>
+    <Swiper slidesPerView={2} spaceBetween={32} navigation={true}  breakpoints={{
+      768:{
+        slidesPerView:3,
+      },
+      1024:{
+        slidesPerView:4,
+      },
+    }}
+    moduless={[Navigation]}
+    className='workoutSlider'
+    >
       {programs.map((program,idx)=>{
         const{image,name}=program
         return <SwiperSlide className='max-w-[320px] max-h-[320px] relative' key={idx}>
          <img src={program.image} alt="" className="object-cover w-full h-full" />
-         <div className="">
-          <div className="">
+         <div className="absolute left-[20px] bottom-[20px]  bg-white h-[26px] px[14px] flex items-center rounded-[1px]">
+          <div className="text-sm font-semibold font-primary text-neutral-500">
             {name}
           </div>
          </div>
