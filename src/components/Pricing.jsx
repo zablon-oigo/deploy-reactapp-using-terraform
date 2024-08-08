0b173f3538fc9ae2ea6f1f419073113b2de63061
zablon <zablonoigo51@gmail.com>
2024-08-08 17:37:34 +0300

    Importing pricing list

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
-
+import {pricing} from '../data'
+import PlanList from './PlanList'
 export default function Pricing() {
+    const{icon,title,plans}=pricing
   return (
-    <div>Pricing</div>
+    <section className='section'>
+        <div className="section-title-group  max-w-[540px] mx-auto px-4 lg:px-0">
+            <img src={icon} alt="" className="" />
+            <h2 className="h2">
+                {title}
+                <span className="text-primary-200">.</span>
+            </h2>
+        </div>
+        <PlanList plans={plans} />
+    </section>
   )
 }
