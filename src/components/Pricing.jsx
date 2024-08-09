ee291f2aef5b5195e00d07499584dbf0f696d940
zablon <zablonoigo51@gmail.com>
2024-08-09 11:52:27 +0300

    Added smooth animation

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -5,7 +5,11 @@ export default function Pricing() {
     const{icon,title,plans}=pricing
   return (
     <section className='section'>
-        <div className="section-title-group  max-w-[540px] mx-auto px-4 lg:px-0">
+        <div className="section-title-group  max-w-[540px] mx-auto px-4 lg:px-0"
+        data-aos='fade-up'
+        data-aos-offset='200'
+        data-aos-delay='200'
+        >
             <img src={icon} alt="" className="" />
             <h2 className="h2">
                 {title}
