b5d472847a20c15f696e4812099d4304e8d99877
zablon <zablonoigo51@gmail.com>
2024-08-10 09:43:54 +0300

    Added community slider

---
+++ src/components/CommunitySlider.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {Swiper,SwiperSlide} from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/navigation'
+export default function CommunitySlider({testimonials}) {
+  return (
+    <Swiper slidesPerView={3} spaceBetween={32}  grabCursor={true}   centeredSlides={'auto'} modules={[
+
+    ]}>
+        {testimonials.map((testimonial,idx)=>{
+            const{image,name,message}=testimonial;
+            return <SwiperSlide key={idx}>
+                <div className="relative">
+                    <div className="">
+                        <img src={image} alt="" className="" />
+                    </div>
+                    <div className="">
+                        <span className="mb-8 text-lg italic font-light">
+                            {message}
+                        </span>
+                        <div className="flex items-center justify-center gap-x-[3px] ">
+                            <span className="text-[30px] text-primary-200 font-bold">~</span>
+                            <span className="text-[20px] font-bold">
+                            {name}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </SwiperSlide>
+        })}
+    </Swiper>
+  )
+}
