f18765fd5c442163e4c4d367ef2810fdc52fdc9d
zablon <zablonoigo51@gmail.com>
2024-08-11 01:16:16 +0300

    Changed component state value

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,10 +1,10 @@
 import React,{useState} from 'react'
 import {FaChevronCircleUp,FaChevronCircleDown} from 'react-icons/fa'
 export default function Accordion({accordion}) {
-  const [isOpen,setIsOpen]=useState(false)
+  const [isOpen,setIsOpen]=useState(true)
   const{question,answer}=accordion
     return (
-    <section className="cursor-pointer">
+    <section  className="cursor-pointer">
         <div className="bg-white border rounded-sm">
             <div className="min-h-[60px] flex items-center justify-between px-[30px]">
                 <h6 className="h6">
