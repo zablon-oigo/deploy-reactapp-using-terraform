bdb7bf70df693ac4d06b7081d0a7ae41ffa5ef3d
zablon <zablonoigo51@gmail.com>
2024-08-11 01:05:33 +0300

    Changed content position

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -2,10 +2,19 @@ import React,{useState} from 'react'
 import {FaChevronCircleUp,FaChevronCircleDown} from 'react-icons/fa'
 export default function Accordion({accordion}) {
   const [isOpen,setIsOpen]=useState(false)
-  const{question,anser}=accordion
+  const{question,answer}=accordion
     return (
     <section className="cursor-pointer">
-        <div className="bg-white border rounded-sm"></div>
+        <div className="bg-white border rounded-sm">
+            <div className="min-h-[60px] flex items-center justify-between px-[30px]">
+                <h6 className="h6">
+                    {question}
+                </h6>
+                <div className="">
+                    {isOpen? <FaChevronCircleUp className='text-[20px]'/>:<FaChevronCircleDown className='text-[20px]'/>}
+                </div>
+            </div>
+        </div>
 
     </section>
   )
