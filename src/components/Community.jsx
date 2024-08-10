2a17d7180b989c99339a48af602abbd72e251c63
zablon <zablonoigo51@gmail.com>
2024-08-10 13:11:35 +0300

    Adding community component

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -6,7 +6,7 @@ export default function Community() {
   return (
     <section className="relative section">
       <div className="container mx-auto">
-          <div className="">
+          <div className="flex">
             <div className="section-title-group  max-w-[240px] px-4 lg:px-0 lg:ml-0 mx-auto">
               <img src={icon} alt="" className="" />
               <h2 className="h2 section-title">
