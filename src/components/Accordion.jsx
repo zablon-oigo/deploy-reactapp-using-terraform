7462aa06f7bd020e66d7bdb6b990a326df2ec7d0
zablon <zablonoigo51@gmail.com>
2024-08-11 01:00:50 +0300

    Added faq section

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {FaChevronCircleUp,FaChevronCircleDown} from 'react-icons/fa'
+export default function Accordion({accordion}) {
+  const [isOpen,setIsOpen]=useState(false)
+  const{question,anser}=accordion
+    return (
+    <section className="cursor-pointer">
+        <div className="bg-white border rounded-sm"></div>
 
-export default function Accordion() {
-  return (
-    <div>Accordion</div>
+    </section>
   )
 }
