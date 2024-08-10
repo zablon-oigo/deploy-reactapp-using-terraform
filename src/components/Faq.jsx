5696086cdeaeb4df0596265479291ec0a8454329
zablon <zablonoigo51@gmail.com>
2024-08-10 13:43:14 +0300

    Adding component title

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import {faq} from '../data'
+import Accordion from './Accordion'
 export default function Faq() {
     const{icon,title,accordions}=faq
   return (
-    <section className="bg-pink-200 section pt-[480px] lg:pt-[280px]">
-        
+    <section className="section pt-[100px] lg:pt-[20px]">
+        <div className="max-w-[768px] mx-auto lg:bg-faq bg-no-repeat bg-custom bg-center lg:h-[1160px] ">
+            <div className="section-title">
+                <img src={icon} alt="" className="lg:hidden" />
+                <h2 className="h2 section-title lg:mt-[100px]">
+                    {title}
+                    <span className="text-primary-200">
+                        .
+                    </span>
+                </h2>
+            </div>
+            <div className="">
+                {accordions.map((accordion,idx)=>{
+                   return(
+                    <Accordion accordion={accordion}  key={idx}/>
+                   )
+                })}
+            </div>
+        </div>
     </section>
   )
 }
