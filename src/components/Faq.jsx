0db37e61ecfe50ad3bf2406ca71ddc7dcef19a75
zablon <zablonoigo51@gmail.com>
2024-08-10 13:12:52 +0300

    FAQ component

---
+++ src/components/Faq.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {faq} from '../data'
+export default function Faq() {
+    const{icon,title,accordions}=faq
+  return (
+    <section className="bg-pink-200 section pt-[480px] lg:pt-[280px]">
+        
+    </section>
+  )
+}
