a9059147af13f578237cc2f993de4c1c0fa85b33
zablon <zablonoigo51@gmail.com>
2024-08-12 22:46:52 +0300

    Aligning items at the center

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
-
+import {join} from '../data'
 export default function Join() {
+  const {image,title,subtitle,btnText}=join;
   return (
-    <div>Join</div>
+    <div className=''>
+        <div className="container mx-auto">
+            <div className="flex flex-col -space-y-24 md:flex-row md:items-center md:-space-x-12 lg:-space-y-0">
+                <div className="">
+                    <div className="">
+                        <img src={image} alt="" className="" />
+                    </div>
+                    <div className="">
+                        <h2 className="">
+                            Want to join
+                            <span className=""></span>
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
