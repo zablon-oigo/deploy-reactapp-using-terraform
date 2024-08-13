66cde1fde4d55b159d3df7f1221aa559b47e4407
zablon <zablonoigo51@gmail.com>
2024-08-13 17:59:28 +0300

    Reduced FAQ max height

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -5,7 +5,7 @@ export default function Faq() {
     const{icon,title,accordions}=faq
   return (
     <section className="section pt-[100px] lg:pt-[20px]">
-        <div className="max-w-[768px] mx-auto lg:bg-faq bg-no-repeat bg-custom bg-center lg:h-[1160px] ">
+        <div className="max-w-[768px] mx-auto lg:bg-faq bg-no-repeat bg-custom bg-center lg:h-[720px] ">
             <div className="justify-start -space-x-4 section-title-group lg:justify-center lg:max-w-[540px] mx-auto px-4 lg:px-0">
                 <img src={icon} alt="" className="lg:hidden" />
                 <h2 className="h2 section-title lg:mt-[100px]">
