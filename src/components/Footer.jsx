a887b45dde3b96f0328c672a027041df14404678
zablon <zablonoigo51@gmail.com>
2024-08-13 18:16:09 +0300

    Added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
-
+import {footer}  from '../data'                          
 export default function Footer() {
+    const{logo,copyrightText}=footer
   return (
-    <div>Footer</div>
+    <div className='container mx-auto'>
+        <a href="" className="">
+            <img src={logo} alt="" className="" />
+        </a>
+        <p className="">
+            {copyrightText}
+        </p>
+    </div>
   )
 }
