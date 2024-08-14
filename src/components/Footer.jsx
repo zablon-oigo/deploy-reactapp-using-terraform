ee7a8abd98c331ef3f47da55f7fea28d4f058376
zablon <zablonoigo51@gmail.com>
2024-08-14 15:15:11 +0300

    Added footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,16 @@ import {footer}  from '../data'
 export default function Footer() {
     const{logo,copyrightText}=footer
   return (
-    <div className='container mx-auto'>
+    <footer className="bg-neutral-500 h-[125px] md:h-[195px] px-[20px] ">
+        <div className='container mx-auto h-full flex justify-between items-center md:items-end md:pb-[50px]'
+        data-aos='fade-up'   data-aos-delay='200'>
         <a href="" className="">
             <img src={logo} alt="" className="" />
         </a>
-        <p className="">
+        <p className="text-sm text-neutral-300">
             {copyrightText}
         </p>
     </div>
+    </footer>
   )
 }
